cb1a83aa38befa48f6e6b455daf6e5cd499b0856
hatlonely <hatlonely@gmail.com>
2019-08-06 04:01:40 +0800

    update

---
+++ web/account/src/components/HFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-footer absolute class="font-weight-medium" app>
+    <v-flex text-center xs12>
+      <div class="body-2">
+        <a href="#">Privacy Policy</a>&nbsp;&nbsp;·&nbsp;&nbsp;
+        <a href="#">Terms of Service</a>&nbsp;&nbsp;·&nbsp;&nbsp;
+        <a href="#">Help</a>
+      </div>
+    </v-flex>
+  </v-footer>
+</template>
+
+<style>
+footer.v-footer a {
+  color: rgba(0, 0, 0, 0.54);
+}
+</style>
+
